b573fcb7ba3b9d663ec8d56c8817bcc330a4b03c
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-11-19 12:02:44 -0300

    Criando iteração.

--- Python/ex/ex003.py
+++ Python/ex/ex003.py
@@ -1,7 +1,9 @@
-nome = str(input('Digite seu nome: '))
-if 4 > len(nome) >= 1:
-    print('Seu nome é pequeno!')
-elif 6 > len(nome) > 4:
-    print('Seu nome é medio!')
-else:
-    print('Seu nome é grande!')
+contlin = 5
+contcol = 5
+linha = 0
+while linha <= contlin:
+    coluna = 0
+    while coluna <= contcol:
+        print(f'{linha}, {coluna}')
+        coluna += 1
+    linha += 1
