4cd71bcef0d485ee3b324fbf31826983f46d5fea
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-11-21 20:09:38 -0300

    ok

--- Python/ex/ex013.py
+++ Python/ex/ex013.py
@@ -1,1 +1,14 @@
-print('OI')
+pessoa = {
+    'nome': 'Luiz',
+    'Sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.80,
+    'endereço':[
+        {'rua': 'Rua das cegonhas', 'numero':229},
+        {'rua': 'Avenida JK','numero': 123}
+    ]
+}
+
+print(pessoa)
+for key, value in pessoa.items():
+    print(key, value)
