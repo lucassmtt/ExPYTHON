5de551deed756974ffae8082f7f7d783bbb17227
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-11-26 20:34:53 -0300

    Criando generator

---
+++ Python/ex/ex025iterable_iterator.py
@@ -0,0 +1,6 @@
+iterable = ['Eu', 'Tenho', '__iter__'] ##iterável
+iterator = iter(iterable) ##tem o __iter__ e __next__
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
