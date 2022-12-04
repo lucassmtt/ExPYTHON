ea29f7eda78bf8466890dbca464762019dcf71e2
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-12-04 09:20:24 -0300

    Update ex030_.py

--- Python/ex/ex030_.py
+++ Python/ex/ex030_.py
@@ -1,12 +1,15 @@
-def soma(x,y):
+def soma(x, y):
     return x + y
 
 
-def multiplica(x,y):
+def multiplica(x, y):
     return x * y
 
-def criar_funcao(funcao, *args):
-    return funcao(*args)
+def criar_funcao(funcao, x):
+    def interna(y):
+        return funcao(x, y)
+    return interna
 
 soma_com_cinco = criar_funcao(soma, 5)
-multiplicador_por_dez = criar_funcao(multiplica, 5, 5)
+multiplicador_por_dez = criar_funcao(multiplica, 10)
+print(soma_com_cinco(3))
