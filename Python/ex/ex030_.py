213759e8839a86dfd89c7d3838d0aaaa5822d207
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-12-03 22:12:23 -0300

    Create ex030_.py

---
+++ Python/ex/ex030_.py
@@ -0,0 +1,12 @@
+def soma(x,y):
+    return x + y
+
+
+def multiplica(x,y):
+    return x * y
+
+def criar_funcao(funcao, *args):
+    return funcao(*args)
+
+soma_com_cinco = criar_funcao(soma, 5)
+multiplicador_por_dez = criar_funcao(multiplica, 5, 5)
