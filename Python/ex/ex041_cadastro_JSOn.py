2bdc0d2740ba1af19ed196ee43a4999e3183c5a2
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-12-15 10:54:07 -0300

    Update ex041_cadastro_JSOn.py

--- Python/ex/ex041_cadastro_JSOn.py
+++ Python/ex/ex041_cadastro_JSOn.py
@@ -1,77 +1,89 @@
-import json
-
-def is_string(msg, aviso=''):
-    while True:
-        string = input(msg)
-        if string.isalpha():
-            string = str(string)
-            break
-        print(aviso)
-    return string
-
-def is_number(msg, aviso=''):
-    while True:
-        number = input(msg)
-        if number.isnumeric():
-            number = int(number)
-            break
-        print(aviso)
-    return number
-
-def underline(n):
-    return print(n* '-')
-
-
-pessoa = {}
-pessoas = {
-
-}
-# pessoa = {
-#     'Nome': '',
-#     'Idade': '',
-#     'Notas': [
+# import json
 #
-#     ]
+# def is_string(msg, aviso=''):
+#     while True:
+#         string = input(msg)
+#         if string.isalpha():
+#             string = str(string)
+#             break
+#         print(aviso)
+#     return string
+#
+# def is_number(msg, aviso=''):
+#     while True:
+#         number = input(msg)
+#         if number.isnumeric():
+#             number = int(number)
+#             break
+#         print(aviso)
+#     return number
+#
+# def underline(n):
+#     return print(n* '-')
+#
+#
+# pessoa = {}
+# pessoas = {
+#     'pessoa_id_0': {
+#
+#     }
 # }
-id = 0
-while True:
-    nome = is_string('Digite seu nome: ', aviso='Digite seu nome corretamente, por favor: ')
-    idade = is_number('Digite sua idade', aviso='Digite sua idade corretamente, por favor: ')
-    numero_notas = is_number('Quantas notas você teve? ')
-    notas = []
-    print(type(numero_notas))
-    cont = 1
-    for nota in range(numero_notas):
-
-        while True:
-            nota_verif = is_number(f'Digite sua nota número {cont}: ')
-            if 0 <= nota_verif <= 10:
-                cont += 1
-                notas.append(nota_verif)
-                break
-            else:
-                print('Por favor digite sua nota, corretamente...')
-    pessoa.setdefault('nome', nome)
-    pessoa.setdefault('idade', idade)
-    pessoa.setdefault('notas', notas)
-    while True:
-        print('1 == SAIR')
-        underline(20)
-        print('2 == CADASTRAR MAIS PESSOAS')
-        underline(20)
-        print('3 == VER PESSOAS CADASTRADAS(ADMIN)')
-        underline(20)
-        resp = is_number('Qual operação você gostaria de fazer? ')
-        if resp == 1:
-            break
-        if resp == 2:
-            continue
-        if resp == 3:
-            while True:
-                login = input('Digite seu login de administrador: ')
-                senha = input('Digite sua senha...')
-                if login == 'MOTTA' and senha == '12345678':
-                    print(pessoas)
-                    break
-    if resp == 1:
-        break
+# # pessoa = {
+# #     'Nome': '',
+# #     'Idade': '',
+# #     'Notas': [
+# #
+# #     ]
+# # }
+# id = 0
+# while True:
+#     nome = is_string('Digite seu nome: ', aviso='Digite seu nome corretamente, por favor: ')
+#     idade = is_number('Digite sua idade', aviso='Digite sua idade corretamente, por favor: ')
+#     numero_notas = is_number('Quantas notas você teve? ')
+#     notas = []
+#     print(type(numero_notas))
+#     cont = 1
+#     for nota in range(numero_notas):
+#
+#         while True:
+#             nota_verif = is_number(f'Digite sua nota número {cont}: ')
+#             if 0 <= nota_verif <= 10:
+#                 cont += 1
+#                 notas.append(nota_verif)
+#                 break
+#             else:
+#                 print('Por favor digite sua nota, corretamente...')
+#     pessoa.setdefault('nome', nome)
+#     pessoa.setdefault('idade', idade)
+#     pessoa.setdefault('notas', notas)
+#     while True:
+#         print('1 == SAIR')
+#         underline(20)
+#         print('2 == CADASTRAR MAIS PESSOAS')
+#         underline(20)
+#         print('3 == VER PESSOAS CADASTRADAS(ADMIN)')
+#         underline(20)
+#         resp = is_number('Qual operação você gostaria de fazer? ')
+#         if resp == 1:
+#             break
+#         if resp == 2:
+#             pessoas.setdefault(nome, )
+#             continue
+#         if resp == 3:
+#             while True:
+#                 login = input('Digite seu login de administrador: ')
+#                 senha = input('Digite sua senha...')
+#                 if login == 'MOTTA' and senha == '12345678':
+#                     print(pessoas)
+#                     break
+#     if resp == 1:
+#         break
+#
+# print(pessoa)
+pessoas = {}
+pessoa = {
+    'nome': 'Lucas'
+}
+cont = 1
+pessoas.setdefault('id_01', pessoa)
+print(pessoas)
