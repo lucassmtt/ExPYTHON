73bf32ed185951be57d106b5d94d49650e450c8c
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-12-15 09:10:39 -0300

    ok

---
+++ Python/ex/ex041_cadastro_JSOn.py
@@ -0,0 +1,8 @@
+# def is_string(msg):
+#     while True:
+#         string = input(msg)
+#         if string.isidentifier() == 'str':
+#             break
+#
+
+# nome = is_string('Digite seu nome:')
