18215550805d70a8ebb59a5655dae309e50d9614
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-12-17 23:45:17 -0300

    Classes...

---
+++ Python/ex/ex043_classes.py
@@ -0,0 +1,25 @@
+# class Pessoa:
+#     def __init__(self, nome, sobrenome):
+#         self.nome = nome
+#         self.sobrenome = sobrenome
+#
+#
+# p1 = Pessoa('Lucas', 'Motta')
+# p2 = Pessoa('Joao', 'Aguiar')
+#
+# print(p1.nome)
+# print(p1.sobrenome)
+# print()
+# print(p2.nome)
+# print(p2.sobrenome)
+
+class Carro:
+    def __init(self, cor, modelo, ano):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+
+
+carro01 = Carro('Amarelo', 'hetch', '1998')
+
+print(carro01.Amarelo)
