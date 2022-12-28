2c5bf7c95fbb4c0c1a282f23385f3f98f577df36
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-12-28 20:41:02 -0300

    .

--- Python/ex/ex074_mais_ainda_json].py
+++ Python/ex/ex074_mais_ainda_json].py
@@ -1,15 +1,28 @@
 import json
 import os
-
-
-NOME_ARQ = 'aulaex074.json'
-ABSOLUTO = os.path.abspath(
-    os.path.join(
-        os.path.dirname(__file__), NOME_ARQ
-    )
-)
-
-
-print(__file__,'file')
-print(NOME_ARQ)
-print(ABSOLUTO)
+#
+#
+# NOME_ARQ = 'aulaex074.json'
+# ABSOLUTO = os.path.abspath(
+#     os.path.join(
+#         os.path.dirname(__file__), NOME_ARQ
+#     )
+# )
+#
+#
+# string_json ={
+#   "title": "O Senhor dos Anéis: A Sociedade do Anel",
+#   "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+#   "is_movie": True,
+#   "imdb_rating": 8.8,
+#   "year": 2001,
+#   "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+#   "budget": None
+# }
+#
+# with open(ABSOLUTO, 'w', encoding='UTF8') as arquivo:
+#     json.dump(string_json, arquivo, indent=2, ensure_ascii=False)
+#
+# with open(ABSOLUTO, 'r+', encoding='UTF8') as arquivo:
+#     movie = json.load(arquivo)
+#     print(movie)
