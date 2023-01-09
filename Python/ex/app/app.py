e8a744322eaa7cadd5cb058dcc90b82d7234d39a
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-09 08:46:03 -0300

    ok

--- Python/ex/app/app.py
+++ Python/ex/app/app.py
@@ -1,3 +1,33 @@
-import os
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-caminho = 
+ROOT = Path(__file__).parent.parent.parent.parent
+CHROME_DRIVER_PATH = ROOT / 'venv' / 'bin' / 'chromedriver'
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+
+    #chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=CHROME_DRIVER_PATH,
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+if __name__ == '__main__':
+    options = ('--disable-gpu', '--no-sandbox',)
+    browser = make_chrome_browser(*options)
+    browser.get('https://www.google.com')
+    browser.quit()
+
---
+++ Python/ex/ex081_Subprocess.py
@@ -0,0 +1,6 @@
+import subprocess
+
+# proc = subprocess.run(
+#     ['ping', '127.0.0.1', '-c', '4']
+# )
+subprocess.run("-")
