fe5b963bfbc31a1f0a60c83d24e6dd14e2e049eb
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-11 09:48:55 -0300

    ok

--- Python/ex/ex081_Subprocess.py
+++ Python/ex/ex081_Subprocess.py
@@ -1,6 +1,7 @@
 import subprocess
 
-# proc = subprocess.run(
-#     ['ping', '127.0.0.1', '-c', '4']
-# )
-subprocess.run("-")
+proc = subprocess.run(
+    ['ping', '127.0.0.1', '-c', '4']
+)
+
+print(proc.stderr)
