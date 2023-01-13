29e580e9f150e539de3b2b584aa056ea121ff0d9
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-12 22:21:39 -0300

    Create app.py

---
+++ Python/appv2/app.py
@@ -0,0 +1,17 @@
+from selenium import *
+#isso não é uma boa prática de programação, 
+#porém nesse caso da para utilizar
+
+key = '123'
+
+#importante conferir se o driver do 
+#selenium está dentro da pasta
+
+class Chrome:
+    def __init__(self, config_drivers)
+        self.config_drivers = config_drivers
+    
+    def...
+
+
+app_chorme = Chrome()
