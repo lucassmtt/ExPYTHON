667c0dc83b017f0158e01efd9247d29409b6704f
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-13 23:58:19 -0300

    Update main.py

--- automatizador/main.py
+++ automatizador/main.py
@@ -1,14 +1,3 @@
-# import pyautogui
-#
-# distance = 300
-#
-# while distance > 0:
-#     pyautogui.drag(distance, 0, duration=0.5)
-#     distance -= 5
-#     pyautogui.drag(0, distance, duration=0.5)
-#     distance -= 5
-#     pyautogui.drag(0, -distance, duration=0.5)
-#     pyautogui.drag(-distance, 0, duration=0.5)
+import pyautogui
 
-nome = str(input('Digite seu nome'))
-print(f'Ola {nome}')
+pyautogui.write('ola')
