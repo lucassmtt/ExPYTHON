5ed456c77a98e68d338a02740d7ef011dfe24ba1
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-14 10:36:23 -0300

    Teste automatizado usando python

--- automatizador/main.py
+++ automatizador/main.py
@@ -1,3 +1,25 @@
 import pyautogui
+from time import sleep
 
-pyautogui.write('ola')
+cont = 0
+while True:
+    pyautogui.moveTo(530, 10)
+    sleep(2)
+    pyautogui.click()
+    sleep(2)
+    pyautogui.moveTo(60, 110)
+    pyautogui.click()
+    sleep(2)
+    pyautogui.moveTo(400, 120)
+    pyautogui.click()
+    sleep(2)
+    pyautogui.write('twitter.com')
+    pyautogui.press('enter')
+    sleep(2)
+    pyautogui.moveTo(800, 300)
+    pyautogui.click()
+    pyautogui.write(f'Este tweet foi escrito em um teste automatizado de python. Tweet numero {cont}')
+    sleep(2)
+    pyautogui.moveTo(1150, 430)
+    pyautogui.click()
+    cont += 1twitter.com
