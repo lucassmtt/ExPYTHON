e17306e40f6147b4d586332f2e79e65c6bccdfa8
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-17 16:45:01 -0300

    Update 12-Classe_conta_investimento.py

--- Python/ex/Exercicios_classes/12-Classe_conta_investimento.py
+++ Python/ex/Exercicios_classes/12-Classe_conta_investimento.py
@@ -1,11 +1,33 @@
-class ContaInvestimento:
-    def __init__(self, numero_conta=0, nome_cliente='', saldo_inical=0, taxa_juros=0):
+class Conta:
+    def __init__(self, numero_conta=0, nome='', saldo=0):
         self.numero = numero_conta
-        self.nome = nome_cliente
-        self.saldo = saldo_inical
-        self.taxa = taxa_juros
+        self.nome = nome
+        self.saldo = saldo
 
-    def add_juros(self, porc_juros):
-        self.taxa = porc_juros
+    def setNome(self, nome):
+        self.nome = nome
 
-    
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def saque(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+
+class contaInvestimento(Conta):
+    def __init__(self, numero, nome, saldo, taxaJuros):
+        super().__init__(numero, nome, saldo)
+        self.taxa_juros = taxaJuros
+
+    def add_juros(self):
+        self.saldo += (self.saldo * self.taxa_juros / 100)
+
+
+poupanca = contaInvestimento(12221, 'Lucas', 1340, 10)
+print(vars(poupanca))
+poupanca.add_juros()
+poupanca.add_juros()
+poupanca.add_juros()
+poupanca.add_juros()
+poupanca.add_juros()
+print(vars(poupanca))
