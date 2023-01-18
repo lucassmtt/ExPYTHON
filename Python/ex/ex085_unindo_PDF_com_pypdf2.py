a7a53f931b01885da8ae0375e353b03eedcda855
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-18 19:11:10 -0300

    Update ex085_unindo_PDF_com_pypdf2.py

--- Python/ex/ex085_unindo_PDF_com_pypdf2.py
+++ Python/ex/ex085_unindo_PDF_com_pypdf2.py
@@ -1,17 +1,31 @@
 import PyPDF2
 import os
 
-CAMINHO_PDF = 'pdf'
+# CAMINHO_PDF = 'pdf'
 
-novo_pdf = PyPDF2.PdfMerger()
+# novo_pdf = PyPDF2.PdfMerger()
+#
+# for root, dirs, files in os.walk(CAMINHO_PDF):
+#     for file in files:
+#         path_pdf_complete = os.path.join(root, file)
+#
+#         with open(path_pdf_complete, 'rb') as arq:
+#             novo_pdf.append(arq)
+#
+#
+# with open(f'{CAMINHO_PDF}/novo_arquivo.pdf', 'wb') as meu_novo_pdf:
+#     novo_pdf.write(meu_novo_pdf)
 
-for root, dirs, files in os.walk(CAMINHO_PDF):
-    for file in files:
-        path_pdf_complete = os.path.join(root, file)
+# with open('pdf/novo_arquivo.pdf', 'rb') as arquivo:
+#     reader = PyPDF2.PdfReader(arquivo)
+#     num_pags = len(reader.pages)
+#     # print(num_pags)
+#
+#     for pag in range(num_pags):
+#         print(pag)
+#         writer = PyPDF2.PdfWriter()
+#         # pagina_atual = writer.getPage(pag) //Deprecated
+#         pagina_atual = writer.pages
 
-        with open(path_pdf_complete, 'rb') as arq:
-            novo_pdf.append(arq)
 
-
-with open(f'{CAMINHO_PDF}/novo_arquivo.pdf', 'wb') as meu_novo_pdf:
-    novo_pdf.write(meu_novo_pdf)
+##Modulo muito antigo, maioria dos métodos estão depreciados 
