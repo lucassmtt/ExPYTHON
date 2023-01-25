6cf8efaf02582a6a34a364c688dd8a3c1ed69aa4
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-01-25 17:17:58 -0300

    Update calculadora.py

--- Python/ex/calculadora.py
+++ Python/ex/calculadora.py
@@ -1,3 +1,4 @@
-primeiro_numero = int(input('Digite um número: '))
-segundo_numero = int(input('Digite outro número: '))
-print(primeiro_numero + segundo_numero)
+# primeiro_numero = int(input('Digite um número: '))
+# segundo_numero = int(input('Digite outro número: '))
+# print(primeiro_numero + segundo_numero)
+
