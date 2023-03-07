7ddf26f00f13d2fd66b77e6f4c091f542ebc9f7d
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-07 19:25:08 -0300

    Update ex091-Ex-intermediario.py

--- Python/ex/ex091-Ex-intermediario.py
+++ Python/ex/ex091-Ex-intermediario.py
@@ -7,6 +7,15 @@ Cada espectador de um cinema respondeu a um questionário no qual constava sua i
 - a quantidade de pessoas que respondeu regular
 - a percentagem de pessoas que respondeu bom entre todos os espectadores analisados
 """
+def valida_opc(msg, msg_error):
+    while True:
+        opc = int(input(msg))
+        if opc == 1 or opc == 2 or opc == 3:
+            break
+        else:
+            print(msg_error)
+
+
 qtd_regular = 0
 
 idade_pessoas_otimo = 0
@@ -16,32 +25,30 @@ media_otimo = 0
 qtd_bom = 0
 qtd_de_espectadores = int(input('Qual é a quantidade de espectadores? '))
 
-opc = 0
 
 for i in range(qtd_de_espectadores):
-    while opc != 1 or 2 or 3:
-        idade = int(input('Digite sua idade'))
-        print("""
-    
-            (1) = Regular
-            (2) = Bom
-            (3) = Ótimo
-            
-        """)
+    idade = int(input('Digite sua idade'))
+    print("""
 
-        opc = int(input('Digite sua opinião'))
-        if opc == 1:
-            qtd_regular += 1
-            break
+        (1) = Regular
+        (2) = Bom
+        (3) = Ótimo
 
-        if opc == 2:
-            qtd_bom += 1
-            break
+    """)
 
-        if opc == 3:
-            idade_pessoas_otimo += idade
-            qtd_pessoas_responderam_otimo += 1
-            break
+    opc = valida_opc('Digite uma opção: ', 'Opção inválida, digite uma opção correta!')
+    if opc == 1:
+        qtd_regular += 1
+        break
+
+    if opc == 2:
+        qtd_bom += 1
+        break
+
+    if opc == 3:
+        idade_pessoas_otimo += idade
+        qtd_pessoas_responderam_otimo += 1
+        break
 
 
 if qtd_pessoas_responderam_otimo > 0:
@@ -52,3 +59,4 @@ porcentagem_bom = (qtd_bom / qtd_de_espectadores) * 100
 print(f'A média da idade das pessoas que responderam ótimo foi igual a {media_otimo}.')
 print(f'A quantidade de pessoas que respondeu regular foi {qtd_regular}.')
 print(f'A porcentagem de pessoas que respondeu bom foi de {porcentagem_bom:.0f}%.')
+
