79b11741fcc2488d5176a3ba01ba4de337af34d0
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-15 15:01:19 -0300

    Update ex094_m.py

--- Python/ex/ex094_m.py
+++
@@ -1,9 +0,0 @@
-def confirma_int():
-    while True:
-        resp = input('Digite algo')
-        if resp.isnumeric():
-            int(resp)
-            break
-    return resp
-
-num = confirma_int()
