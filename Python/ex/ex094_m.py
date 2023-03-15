d233a0a87dcafe4ddaaa04fbd6a755b0f5f51630
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-15 15:02:23 -0300

    Update ex094_m.py
    Add descrição do exercício.

---
+++ Python/ex/ex094_m.py
@@ -0,0 +1,14 @@
+"""
+Descrição do exercício:
+
+Uma radio necessita realizar alguns cálculos estatísticos referente
+ao número de minutos de musicas e de propagandas que são vinculadas.
+Considere que a radio fica 744 horas por mês no ar.
+Solicite POR SEMANA o número horas da programação em que foram vinculadas músicas
+e o numero de horas em que foram vinculadas apenas propagandas. Calcule e mostre:
+a. A semana em que houve o a maior quantidade de horas de propagandas vinculadas.
+b. A semana em que houve a menor quantidade de horas de musicas vinculadas.
+c. O total de horas de propagandas vinculadas.
+d. O total de horas de musicas vinculadas.
+e. O percentual que o total de horas de propagandas representa do numero total de horas que a rádio fica no ar
+"""
