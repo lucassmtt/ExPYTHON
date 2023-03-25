e941cc878ef2ee2462609987b02f265226c9cc90
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-25 13:25:29 -0300

    Exercício sobre programa que...
    Descrição do exercício:
    
    Faça um programa que receba a quantidade de peças vendidas por vendedor e
    armazene essas quantidades em um vetor.
    Receba também o preço da peça vendida de cada vendedor e
    armazene esses preços em outro vetor.
    Existem apenas dez vendedores e cada vendedor pode vender apenas um
    tipo de peça, isto é, para cada vendedor existe apenas um preço.
    Calcule e mostre a quantidade total de peças vendidas por todos os vendedores
    e para cada vendedor calcule e mostre o valor total da venda, isto é,
    a quantidade de peças * o preço da peça.

---
+++ Python/Ex-Vetores/Vetor6.py
@@ -0,0 +1,15 @@
+"""
+Descrição do exercício:
+
+Faça um programa que receba a quantidade de peças vendidas por vendedor e
+armazene essas quantidades em um vetor.
+Receba também o preço da peça vendida de cada vendedor e
+armazene esses preços em outro vetor.
+Existem apenas dez vendedores e cada vendedor pode vender apenas um
+tipo de peça, isto é, para cada vendedor existe apenas um preço.
+Calcule e mostre a quantidade total de peças vendidas por todos os vendedores
+e para cada vendedor calcule e mostre o valor total da venda, isto é,
+a quantidade de peças * o preço da peça.
+
+"""
+
